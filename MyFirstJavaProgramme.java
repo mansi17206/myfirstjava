071690a10f23cde13adfcb52a7facde7840032ad
Mansi Trivedi <Mansi17206@yahoo.com>
2022-04-02 12:02:16 +0100

    this is my first java programme

---
+++ MyFirstJavaProgramme.java
@@ -0,0 +1,32 @@
+package com.learning.java;
+
+public class MyFirstJavaProgramme {
+
+    public static void main(String[] args) {
+        System.out.println("Mansi Trivedi");
+        System.out.println("Software Basics");
+        System.out.println("Risk of Software");
+        System.out.println("Definition Software Testing");
+        System.out.println("Risk of Software Testing");
+        System.out.println("Risk of Software Testing");
+        System.out.println("Software Developement Life Cycle");
+        System.out.println("Software testing Life Cycle");
+        System.out.println("Qaulification of Tester");
+        System.out.println("Challenges of Manual Testing");
+        System.out.println("Risk of Software Testing");
+        System.out.println("Test Plan");
+        System.out.println("Risk of Software Testing");
+        System.out.println("Test Scenrio");
+        System.out.println("Testing Techniques");
+        System.out.println("Bug and Bug life cycle");
+        System.out.println("SeverityvsPriority");
+        System.out.println("Test Scenrio");
+        System.out.println("Testing levels");
+     
+
+System.out.println("Software Developement Models");
+   System.out.println("Automation Testining");
+
+   System.out.println("Difference between Automation testing and Manual testing");
+   System.out.println("Java and IDE"); }
+}
